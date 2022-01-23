ad84e5351bf57e850b77a1ea91f7bc07492129c0
famxuanlinh <famxuanlinh@gmail.com>
2022-01-23 08:05:00 +0700

    Js

--- ReactJS.js
+++ ReactJS.js
@@ -1,2 +1,47 @@
 // Khoá học Reactjs của F8
+/**
+ 1. 
+ 2. Template Literals
+ 3. Multi-line String
+ 4. Arrow function
+ 5. Classes
+ 6. Enhanced object literals
+ 7. Default parameter values
+ 8. Destructuring
+ 9. Rest parameters
+ 10. Spread
+ 11. Tagged template literal
+ 12. Modules
+ */
+
+ // 6. Enhanced object literals
+ // 6.1 Định nghĩa key: value cho object
+ // 6.2 Định nghĩa method cho object
+ // 6.3 Định nghĩa key cho object dưới
+//  var name = 'Javascript';
+//  var price =  1000;
+//  var cource = {
+//      name, //name: name,
+//      price //
+//      price: price
+//  };
+//  console.log (cource)
+
+//   9. Rest parameter // Lấy những phần tử còn lại
+//   Vd. function logger(a, b,  ...params){
+//       console.log(params);  // Còn lại => [3, 4]
+//   }
+//   logger(1, 2, 3, 4)
+
+
+// 10. Spead  // Nối các phẩn tử/ mảng lại với nhau và bỏ dấu []
+// Vd. 
+var array1 = ['Javascript', 'Ruby', 'PHP']
+var array2 = ['ReactJS', 'Dart']
+var array3 = [...array1, ...array2]
+console.log (array3)
+
+
+
+
 
